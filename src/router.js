fb372c20daf079b92e7b3c8f480a044ea0a09261
CaioCesarLima <cesarlimacaio@gmail.com>
2019-03-09 07:48:22 -0300

    Criado tela de configurações e adicionada nas rotas

--- src/router.js
+++ src/router.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Config from './views/Config'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/config',
+      name: '/config',
+      component: Config
     },
     {
       path: '/about',
